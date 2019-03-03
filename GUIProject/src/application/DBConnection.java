a1a96a40dc488d58c7e007d38529c8f704a056b9
Dark-Hunter752 <47715383+Dark-Hunter752@users.noreply.github.com>
2019-03-03 15:34:53 +0200

    Fix Project setup using eclipse tool

---
+++ GUIProject/src/application/DBConnection.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+	public static Connection connect() {
+		Connection con = null;
+		try {
+			System.out.println("what are trying to do ");
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			String m_Connection = "jdbc:sqlserver://localhost:1433;databaseName=Project_Prototype;user=pro;password=123;";
+			//String m_Connection = "jdbc:sqlserver://Mohamad\\MOHAMADSQL:1433;databaseName=Project_Prototype;user=mohamad;password=123;";
+
+			con = DriverManager.getConnection(m_Connection);
+
+		} catch (Exception e) {
+			AlertController.alert("Database Connection Error","Error");
+		}
+		return con;
+
+	}
+
+}
---
+++ GUIProject/src/application/Main.java
@@ -0,0 +1,73 @@
+
+package application;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+public class Main extends Application {
+	private static Stage mystage;
+	private static int i = 0;
+
+	@Override
+	public void start(Stage primaryStage) {
+		mystage = primaryStage;
+		mystage.setTitle("Inssurance System");
+		mystage.getIcons().add(new Image("/img/icon-quality-assurance.png"));
+		mystage.setResizable(false);
+		// System.out.println("im in and stage is "+mystage);
+		try {
+			init();
+		} catch (Exception e1) {
+	
+			Stage stage = (Stage) mystage.getScene().getWindow();
+			AlertController.alertExc(e1.getMessage(), (Exception) e1, stage);
+		}
+	}
+
+	public static void start2(Stage primaryStage, Scene scene) {
+		try {
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch (Exception e1) {
+			// e.printStackTrace();
+			Stage stage = (Stage) mystage.getScene().getWindow();
+			AlertController.alertExc(e1.getMessage(), (Exception) e1, stage);
+		}
+	}
+
+	public static void SwitchScene(Scene scene) {
+		start2(mystage, scene);
+	}
+
+	public void init() {
+		// Parent root=FXMLLoader.load(getClass().getResource("/fxml/testy.fxml"));
+		// Parent root=FXMLLoader.load(getClass().getResource("testy.fxml"));
+		// Scene scene = new Scene(root);
+		// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		// System.out.println("scene is "+scene);
+		// System.out.println("sttage is q"+ mystage);
+		try {
+			if (i != 0) {
+				SceneFactory.getBackButton().add("Start.fxml");
+				start2(mystage, new SceneFactory().GetRecentScene("Start.fxml"));
+			}
+			i++;
+		} catch (Exception e1) {
+			Stage stage = (Stage) mystage.getScene().getWindow(); // cant catch email syntax
+			AlertController.alertExc(e1.getMessage(), (Exception) e1, stage);
+		}
+	}
+
+	public static void main(String[] args) {
+	
+		launch(args);
+		
+	}
+
+	public Stage getStage() {
+		return mystage;
+	}
+}
